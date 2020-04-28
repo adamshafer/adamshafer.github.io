27953a230d2437e7a06edf9665f52c38b9c16d28
Adam Shafer <adam@adamshafer.com>
2020-04-28 00:22:26 -0500

    Updates

---
+++ precache-manifest.93d9fa3aa1a4c538ef405c63f8d739a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30443de4bcd8305f9947b57bc444500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "652d481f354c2e2458de",
+    "url": "/static/css/main.99a2d4a8.chunk.css"
+  },
+  {
+    "revision": "b1585c1e425c89201575",
+    "url": "/static/js/2.de1654dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de1654dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652d481f354c2e2458de",
+    "url": "/static/js/main.3da90558.chunk.js"
+  },
+  {
+    "revision": "aee280a64715d4b72de4",
+    "url": "/static/js/runtime-main.9d57b4b5.js"
+  }
+]);
