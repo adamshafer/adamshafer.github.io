cdfa89d7279d95e068d82cae2913094c09b60dcd
Adam Shafer <ashafer@warehousequote.com>
2025-03-21 15:35:57 -0500

    trying again to reset

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string;
-}>();
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
 </script>
 
 <template>
@@ -11,7 +14,6 @@ defineProps<{
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-      What's next?
     </h3>
   </div>
 </template>
